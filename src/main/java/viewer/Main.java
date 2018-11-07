82730bdc48ed3748fb98ef51982a7643e63e5ebb
l17012906 <josselin.lecerf@etu.univ-amu.fr>
2018-11-07 11:03:13 +0100

    Test fait sur : — Complex(double real, double imaginary) (constructeur) — double getImaginary() (méthode) — double getReal() (méthode) — static Complex ZERO (constante) — static Complex ONE (constante) — static Complex I (constante)

--- src/main/java/viewer/Main.java
+++ src/main/java/viewer/Main.java
@@ -10,6 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
         Parent root = FXMLLoader.load(getClass().getResource("/viewer/viewer.fxml"));
         primaryStage.setTitle("Mandelbrot");
         primaryStage.setScene(new Scene(root, 1200, 900));
@@ -18,6 +19,7 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+        System.out.println("coucou");
         launch(args);
     }
 }
